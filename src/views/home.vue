ed9b2abe9ac60e28b411cdfd49d7d1d6511debb4
耿连龙 <654506379@qq.com>
2023-12-25 15:20:43 +0800

    refactor: 历史故事线改为ts

--- src/views/home.vue
+++ src/views/home.vue
@@ -2,7 +2,7 @@
  * @Author: 耿连龙 654506379@qq.com
  * @Date: 2023-12-22 10:10:35
  * @LastEditors: 耿连龙 654506379@qq.com
- * @LastEditTime: 2023-12-25 15:07:18
+ * @LastEditTime: 2023-12-25 15:17:12
  * @FilePath: \Warfare-Simulation-Spring\src\views\home.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -13,7 +13,7 @@
         <el-step v-for="item in steps" :key="item.key" :title="item.abstract" @click='handleClick(item)' />
       </el-steps>
       <div v-show="!showSteps" class="content">{{ steps[active] && steps[active].title }}
-        <SvgIcon name="back" size="40px" @click="goBack"/>
+        <SvgIcon name="back" size="40px" @click="goBack" />
       </div>
     </div>
   </map-container>
@@ -23,32 +23,12 @@
 import { ref, reactive, computed } from "vue"
 import MapContainer from "@/components/MapContainer.vue";
 import { ElSteps } from 'element-plus'
+import timeline from "@/assets/js/historyTimeline.js";
 
 const active = ref(-1)
 const showSteps = ref(true)
 const showTips = ref(false)
-const steps = reactive([
-  {
-    title: "公元前522年(周景王二十三年)春，楚平王驱逐太子建、杀死太傅伍奢，伍奢之子伍子胥出奔吴国。",
-    key: "BC522",
-    abstract: "伍子胥出逃"
-  },
-  {
-    title: "公元前519年(周敬王元年)七月，吴、楚战于鸡父（今河南固始县东南），楚军大败。吴王僚攻取楚国州来（今安徽凤台）。",
-    key: "BC519",
-    abstract: "吴王僚大败楚军"
-  },
-  {
-    title: "公元前518年(周敬王二年)冬，楚平王派遣水师入侵吴国，与越军在豫章之纳会师。吴王僚发兵报复楚国，攻占巢、钟离。",
-    key: "BC518",
-    abstract: "吴楚乱战"
-  },
-  {
-    title: "公元前515年(周敬王五年)春，吴王僚派遣公子掩馀、公子烛庸率军伐楚，包围潜地。楚国令尹囊瓦率军进抵沙纳，左尹伯邻宛、工尹寿率军救援潜地。夏，公子光指使专诸刺杀吴王僚。公子光自立为王，是为吴王阖间。",
-    key: "BC515",
-    abstract: "专诸刺杀吴王僚"
-  }
-])
+const steps = reactive(timeline)
 
 const stepWidth = 1 / steps.length * 100 + "%"
 
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,12 +1,13 @@
 /*
  * @Author: 耿连龙 genglianlong@mti-sh.cn
  * @Date: 2023-12-11 16:14:46
- * @LastEditors: 耿连龙 genglianlong@mti-sh.cn
- * @LastEditTime: 2023-12-12 15:21:15
+ * @LastEditors: 耿连龙 654506379@qq.com
+ * @LastEditTime: 2023-12-25 15:18:16
  * @FilePath: \vue3-cesium\src\vite-env.d.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 /// <reference types="vite/client" />
+declare module '*.js'
 interface ImportMetaEnv {
   readonly VITE_APP_TITLE: string;
   // 更多环境变量...
